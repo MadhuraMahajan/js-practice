833e25ec8e4de993a5329e6f1d7c23d6947a101d
MadhuraMahajan <134643083+MadhuraMahajan@users.noreply.github.com>
2023-10-06 06:13:11 +0000

    Done with datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use srtrict"; //treat all js code aa a newwer version
+let age=45;
+
+
+//Predefinrd data types
+//number =>2 to power 52
+//bigint =>use for large value
+// string =""
+// bollean=> true/false
+// null =>stand alone value 
+//undefined =>not speicfy
+//symbol =>unique
+
+
+//object
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,12 +2,13 @@ const accountId = 1563654
 let accountEmail="madhura@gg.com"
 var accountPassword="12345"
 accountCity="Hydrabad"
+let accountState
 
 // accountId=232 not allowed to overwritte
 console.log(accountId);
-
+// prefer not to use var because of use in block scope and functional scope
 accountCity="bengaluru"
 accountPassword="223344"
 accountEmail="gg@mm.com"
 
-console.table([accountId, accountPassword,accountEmail])
+console.table([accountId, accountPassword,accountEmail,accountState])
