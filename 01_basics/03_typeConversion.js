6e89ca1e254d039b8897c503e8fba4e390a4b3af
MadhuraMahajan <134643083+MadhuraMahajan@users.noreply.github.com>
2023-10-06 06:42:34 +0000

    Conversion of data types

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,37 @@
+//  string to number 
+ let  score = "22abc"
+
+ console.log(typeof(score))
+
+ let valueNumber = Number(score)
+ console.log(typeof(valueNumber))
+ console.log(valueNumber)
+
+//  null to number 
+ let grade= null
+
+ console.log(grade)
+ console.log(typeof(grade))
+
+ let gradeNumber= Number(grade)
+ console.log(typeof(gradeNumber))
+ console.log(gradeNumber)
+
+ //boolean to number
+ let value= true
+
+ console.log(value)
+ console.log(typeof(value))
+
+ let vr = Number(value)
+
+ console.log(vr)
+ console.log(typeof(vr))
+
+ let val = undefined
+ console.log(val)
+ console.log(typeof(val))
+
+ let valNum = Number(val)
+ console.log(valNum)
+ console.log(typeof(valNum))
